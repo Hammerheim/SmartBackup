496671390ac05b2bd91c4a6fca845f8c746f4f32
Jacob Vibe Hammer <jacob.vibe.hammer@hotmail.com>
2017-01-30 23:23:59 +0100

    Clean up

---
+++ SmartBackup/Target/InsertFileException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Vibe.Hammer.SmartBackup
+{
+  [Serializable]
+  internal class InsertFileException : Exception
+  {
+    public InsertFileException()
+    {
+    }
+
+    public InsertFileException(string message) : base(message)
+    {
+    }
+
+    public InsertFileException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected InsertFileException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+  }
+}
